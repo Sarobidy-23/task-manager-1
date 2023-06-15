eb1613ac407c498b1029261cc735e904e2992e0c
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-06-15 11:27:15 +0300

    feat: localstorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,19 @@
 const useLocalStorage = () => {
-
+  const getSimpleItem = (title: string) => {
+    return localStorage.getItem(title) as string
+  }
+  const getJSONItem = (title: string) => {
+    return JSON.parse(localStorage.getItem(title) as string)
+  }
+  const addSimpleItem = (title: string, value: string) => {
+    localStorage.setItem(title, value)
+    return {title, value}
+  }
+  const addJSONItem = (title: string, value: {}) => {
+    localStorage.setItem(title, JSON.stringify(value))
+    return {title, value}
+  }
+  return {getSimpleItem, getJSONItem, addSimpleItem, addJSONItem}
 }
 
 export {
